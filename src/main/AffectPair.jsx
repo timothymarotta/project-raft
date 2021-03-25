fd19575756d44365107c4a94e3bc20a36c0d55d4
TimMarotta <timothy.marotta@yahoo.com>
2021-03-25 00:16:13 -0400

    Initial push; created tests for AffectPair

---
+++ src/main/AffectPair.jsx
@@ -0,0 +1,7 @@
+export const AffectPair = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/test/AffectPair.test.js
@@ -0,0 +1,30 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import {AffectPair} from '../main/AffectPair';
+import {testImage} from '../assets/neutral-icon.png';
+
+describe('Affect Pair', () => {
+    test('test button', () => {
+        const mockTriggerConfirmation = jest.fn();
+
+        render(<AffectPair emotion={"Stressed"} image={testImage} triggerConfirmation={mockTriggerConfirmation}/>);
+        screen.debug();
+        expect(screen.getByRole('button')).toBeEnabled();
+
+        userEvent.click(screen.getByRole("button"));
+        expect(mockTriggerConfirmation).toBeCalledTimes(1);
+        expect(screen.getByRole("button")).toBeEnabled();
+
+        expect(screen.getByRole('button').innerText).toBe("Stressed");
+    })
+
+    test('test image', () => {
+        const mockTriggerConfirmation = jest.fn();
+
+        render(<AffectPair emotion={"Stressed"} image={testImage} triggerConfirmation={mockTriggerConfirmation}/>);
+        screen.debug();
+
+        expect(screen.getAllByRole('img').length).toBe(1);
+    })
+})
